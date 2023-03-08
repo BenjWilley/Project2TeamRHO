ce23a575f0df6761af03255e2448e504b5773525
benjwilley <ben@wwilley.com>
2023-03-07 23:13:00 -0600

    changing variable names

--- RestaurantPOS.java
+++ RestaurantPOS.java
@@ -5,8 +5,8 @@ import java.awt.event.*;
 
 public class RestaurantPOS extends JFrame {
 
-    private JButton nextPageButton1;
-    private JButton nextPageButton2;
+    private JButton serverButton;
+    private JButton managerButton;
     private Connection conn;
 
     public RestaurantPOS() {
@@ -29,62 +29,62 @@ public class RestaurantPOS extends JFrame {
         }
 
         // Initialize the buttons
-        nextPageButton1 = new JButton("Server Menu");
-        nextPageButton2 = new JButton("Manager Menu");
+        serverButton = new JButton("Server Menu");
+        managerButton = new JButton("Manager Menu");
 
         // Set the preferred size of the buttons
-        nextPageButton1.setPreferredSize(new Dimension(100, 50));
-        nextPageButton2.setPreferredSize(new Dimension(100, 50));
+        serverButton.setPreferredSize(new Dimension(100, 50));
+        managerButton.setPreferredSize(new Dimension(100, 50));
 
         // Add an ActionListener to the first button
-        nextPageButton1.addActionListener(new ActionListener() {
+        serverButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Create a new frame for the next page 1
-                JFrame nextPageFrame1 = new JFrame("Server");
-                nextPageFrame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                JFrame server = new JFrame("Server");
+                server.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 
                 // Add a back button to the next page 1
                 JButton backButton1 = new JButton("Back");
                 backButton1.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
-                        nextPageFrame1.dispose();  // Close the next page 1 frame
+                        server.dispose();  // Close the next page 1 frame
                         setVisible(true);  // Show the main frame
                     }
                 });
-                nextPageFrame1.add(backButton1, BorderLayout.SOUTH);  // Add the back button to the bottom of the frame
+                server.add(backButton1, BorderLayout.SOUTH);  // Add the back button to the bottom of the frame
                 
-                nextPageFrame1.setVisible(true);
-                nextPageFrame1.setSize(1000, 600);
-                nextPageFrame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                nextPageFrame1.setLocationRelativeTo(null);
-                nextPageFrame1.setVisible(true);
+                server.setVisible(true);
+                server.setSize(1000, 600);
+                server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                server.setLocationRelativeTo(null);
+                server.setVisible(true);
                 
                 setVisible(false);  // Hide the main frame
             }
         });
 
         // Add an ActionListener to the second button
-        nextPageButton2.addActionListener(new ActionListener() {
+        managerButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 // Create a new frame for the next page 2
-                JFrame nextPageFrame2 = new JFrame("Manager");
-                nextPageFrame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                JFrame manager = new JFrame("Manager");
+                manager.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 
                 // Add a back button to the next page 2
                 JButton backButton2 = new JButton("Back");
                 backButton2.addActionListener(new ActionListener() {
                     public void actionPerformed(ActionEvent e) {
-                        nextPageFrame2.dispose();  // Close the next page 2 frame
+                        manager.dispose();  // Close the next page 2 frame
                         setVisible(true);  // Show the main frame
                     }
                 });
-                nextPageFrame2.add(backButton2, BorderLayout.SOUTH);  // Add the back button to the bottom of the frame
+                manager.add(backButton2, BorderLayout.SOUTH);  // Add the back button to the bottom of the frame
                 
-                nextPageFrame2.setVisible(true);
-                nextPageFrame2.setSize(1000, 600);
-                nextPageFrame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                nextPageFrame2.setLocationRelativeTo(null);
-                nextPageFrame2.setVisible(true);
+                manager.setVisible(true);
+                manager.setSize(1000, 600);
+                manager.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                manager.setLocationRelativeTo(null);
+                manager.setVisible(true);
                 
                 setVisible(false);  // Hide the main frame
             }
@@ -97,8 +97,8 @@ public class RestaurantPOS extends JFrame {
         buttonPanel.setLayout(new GridLayout(3, 1, 10, 10));
 
         // Add the buttons to the panel
-        buttonPanel.add(nextPageButton1);
-        buttonPanel.add(nextPageButton2);
+        buttonPanel.add(serverButton);
+        buttonPanel.add(managerButton);
 
         // Add the panel to the frame
         this.add(buttonPanel, BorderLayout.CENTER);
